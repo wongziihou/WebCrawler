bb12412b2fa103714a8ebd98a223113e70e2b8be
Zvonimir <wongziihou@users.noreply.github.com>
2017-03-26 19:47:33 +0800

    Add files via upload

---
+++ getjpg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#coding=utf-8
+
+
+import re
+import urllib
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre,html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl,'%s.jpg' % x)
+        x+=1
+
+html = getHtml('https://www.taobao.com/')
+print html
+
